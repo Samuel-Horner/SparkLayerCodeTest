5c4800f324820c5a4f76307458d3e33607b1e002
Samuel Horner <samkoseyhorner@gmail.com>
2025-11-15 15:20:31 +0000

    Implemented GET handling on the server

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,55 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+type ToDo struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type ToDoList []ToDo
+
+const port int = 8080
+
+var toDoList ToDoList = ToDoList{}
 
 func main() {
 	// Your code here
+	http.HandleFunc("/", ToDoListHandler)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Returning list: ", toDoList)
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(toDoList)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Method not allowed: ", r.Method)
+	w.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func ToDoListHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	// Your code here
+	log.Print("Recieved ", r.Method, " for ", r.URL.Path, " from ", r.RemoteAddr)
+
+	switch r.Method {
+	case http.MethodGet:
+		getHandler(w, r)
+	// case http.MethodPost:
+	// 	postHandler(w, r)
+	default:
+		defaultHandler(w, r)
+	}
 }
